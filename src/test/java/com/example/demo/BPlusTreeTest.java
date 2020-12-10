7311a02db49df57d80898ccb77fc4a96a91f8596
zk <zhengke@rgbinfor.com>
2020-12-10 17:50:50 +0800

    B+Tree

---
+++ src/test/java/com/example/demo/BPlusTreeTest.java
@@ -0,0 +1,173 @@
+package com.example.demo;
+
+import com.example.demo.structure.tree.BPlusTree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * @ClassName BPlusTreeTest
+ * @Description: TODO
+ * @Author zk
+ * @Date 2020/12/10
+ **/
+public class BPlusTreeTest {
+    // 测试
+    public static void main(String[] args) {
+
+        int size = 10;
+        int order = 3;
+        testRandomInsert(size, order);
+
+        testOrderInsert(size, order);
+
+        testRandomSearch(size, order);
+
+        testOrderSearch(size, order);
+
+        testRandomRemove(size, order);
+
+        testOrderRemove(size, order);
+    }
+
+    private static void testOrderRemove(int size, int order) {
+        BPlusTree<Integer, Integer> tree = new BPlusTree<Integer, Integer>(order);
+        System.out.println("\nTest order remove " + size + " datas, of order:"
+                + order);
+        System.out.println("Begin order insert...");
+        for (int i = 0; i < size; i++) {
+            tree.insertOrUpdate(i, i);
+        }
+        tree.printBPlusTree();
+        System.out.println("Begin order remove...");
+        long current = System.currentTimeMillis();
+        for (int j = 0; j < size; j++) {
+            if (tree.remove(j) == null) {
+                System.err.println("得不到数据:" + j);
+                break;
+            }
+        }
+        long duration = System.currentTimeMillis() - current;
+        System.out.println("time elpsed for duration: " + duration);
+        tree.printBPlusTree();
+        System.out.println(tree.getHeight());
+    }
+
+    private static void testRandomRemove(int size, int order) {
+        BPlusTree<Integer, Integer> tree = new BPlusTree<Integer, Integer>(order);
+        System.out.println("\nTest random remove " + size + " datas, of order:"
+                + order);
+        Random random = new Random();
+        boolean[] a = new boolean[size + 10];
+        List<Integer> list = new ArrayList<Integer>();
+        int randomNumber = 0;
+        System.out.println("Begin random insert...");
+        for (int i = 0; i < size; i++) {
+            randomNumber = random.nextInt(size);
+            a[randomNumber] = true;
+            list.add(randomNumber);
+            tree.insertOrUpdate(randomNumber, randomNumber);
+        }
+        tree.printBPlusTree();
+        System.out.println("Begin random remove...");
+        long current = System.currentTimeMillis();
+        for (int j = 0; j < size; j++) {
+            randomNumber = list.get(j);
+            if (a[randomNumber]) {
+                if (tree.remove(randomNumber) == null) {
+                    System.err.println("得不到数据:" + randomNumber);
+                    break;
+                } else {
+                    a[randomNumber] = false;
+                }
+            }
+        }
+        long duration = System.currentTimeMillis() - current;
+        System.out.println("time elpsed for duration: " + duration);
+        tree.printBPlusTree();
+        System.out.println(tree.getHeight());
+    }
+
+    private static void testOrderSearch(int size, int order) {
+        BPlusTree<Integer, Integer> tree = new BPlusTree<Integer, Integer>(order);
+        System.out.println("\nTest order search " + size + " datas, of order:"
+                + order);
+        System.out.println("Begin order insert...");
+        for (int i = 0; i < size; i++) {
+            tree.insertOrUpdate(i, i);
+        }
+        tree.printBPlusTree();
+        System.out.println("Begin order search...");
+        long current = System.currentTimeMillis();
+        for (int j = 0; j < size; j++) {
+            if (tree.get(j) == null) {
+                System.err.println("得不到数据:" + j);
+                break;
+            }
+        }
+        long duration = System.currentTimeMillis() - current;
+        System.out.println("time elpsed for duration: " + duration);
+    }
+
+    private static void testRandomSearch(int size, int order) {
+        BPlusTree<Integer, Integer> tree = new BPlusTree<Integer, Integer>(order);
+        System.out.println("\nTest random search " + size + " datas, of order:"
+                + order);
+        Random random = new Random();
+        boolean[] a = new boolean[size + 10];
+        int randomNumber = 0;
+        System.out.println("Begin random insert...");
+        for (int i = 0; i < size; i++) {
+            randomNumber = random.nextInt(size);
+            a[randomNumber] = true;
+            tree.insertOrUpdate(randomNumber, randomNumber);
+        }
+        tree.printBPlusTree();
+        System.out.println("Begin random search...");
+        long current = System.currentTimeMillis();
+        for (int j = 0; j < size; j++) {
+            randomNumber = random.nextInt(size);
+            if (a[randomNumber]) {
+                if (tree.get(randomNumber) == null) {
+                    System.err.println("得不到数据:" + randomNumber);
+                    break;
+                }
+            }
+        }
+        long duration = System.currentTimeMillis() - current;
+        System.out.println("time elpsed for duration: " + duration);
+    }
+
+    private static void testRandomInsert(int size, int order) {
+        BPlusTree<Integer, Integer> tree = new BPlusTree<Integer, Integer>(order);
+        System.out.println("\nTest random insert " + size + " datas, of order:"
+                + order);
+        Random random = new Random();
+        int randomNumber = 0;
+        long current = System.currentTimeMillis();
+        for (int i = 0; i < size; i++) {
+            randomNumber = random.nextInt(size * 10);
+            System.out.print(randomNumber + " ");
+            tree.insertOrUpdate(randomNumber, randomNumber);
+        }
+        long duration = System.currentTimeMillis() - current;
+        System.out.println("time elpsed for duration: " + duration);
+        tree.printBPlusTree();
+        System.out.println(tree.getHeight());
+    }
+
+    private static void testOrderInsert(int size, int order) {
+        BPlusTree<Integer, Integer> tree = new BPlusTree<Integer, Integer>(order);
+        System.out.println("\nTest order insert " + size + " datas, of order:"
+                + order);
+        long current = System.currentTimeMillis();
+        for (int i = 0; i < size; i++) {
+            tree.insertOrUpdate(i, i);
+        }
+        long duration = System.currentTimeMillis() - current;
+        System.out.println("time elpsed for duration: " + duration);
+        tree.printBPlusTree();
+    }
+
+}
